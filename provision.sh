232247acb5513e331ac52660d458582a1334253b
srinivasarao2468 <34686037+srinivasarao2468@users.noreply.github.com>
2022-06-27 18:58:43 +0530

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,4 +1,11 @@
 TFVARS="terraform.tfvars"
+
+begin_init_block
+env_vars <<'EOF'
+COLOUR
+EOF
+end_init_block
+
 cat << EOF > "${TFVARS}"
 s35_bucket_name="sample123sample"
 s3_bucket_name="sample123sample123"
